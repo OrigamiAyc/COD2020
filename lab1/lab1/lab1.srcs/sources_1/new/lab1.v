70c9f9c9ed7a8907e43f208cffcf6938d470c67b
Lapland Stark <L.G.Stark@outlook.com>
2020-04-25 05:37:32 +0000

    upload lab 1 v

---
+++ lab1/lab1/lab1.srcs/sources_1/new/lab1.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/04/25 04:18:37
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU
+    #(parameter WIDTH = 32) 	// data width
+(
+    output reg [WIDTH-1:0] y,   // calculation result
+    output reg zf,              // zero sign
+    output reg cf,              // jinwei sign
+	output reg of,				// yichu
+	input [WIDTH-1:0] a,b
+	input [2:0] m				// type
+);
+
+	always @(*) begin
+		{zf,of,cf} = 3'b000;
+		case (m)
+			3'b000:  begin
+				{cf, y} = a + b;
+				of = (~a[WIDTH-1] & ~b[WIDTH-1] & y[WIDTH-1]) | (a[WIDTH-1] & b[WIDTH-1] & ~y[WIDTH-1]);
+				zf = ~|y;
+			end
+			3'b001: begin
+				{cf, y} = a - b;
+				of = (~a[WIDTH-1] & b[WIDTH-1] & y[WIDTH-1]) | (a[WIDTH-1] & ~b[WIDTH-1] & ~y[WIDTH-1]);
+				zf = ~|y;
+			end
+			3'b010: begin
+				y = a & b;
+				zf = ~|y;
+			end
+			3'b011: begin
+				y = a | b;
+				zf = ~|y;
+			end
+			3'b100: begin
+				y = a ^ b;
+				zf = ~|y;
+			end
+			default: y = 
+		endcase
+	end
+endmodule

3b9589c7a9d853ee5dd23d60d0c6c199d0a85e43
Lapland Stark <L.G.Stark@outlook.com>
2020-04-28 15:37:51 +0000

    upload

--- 实验文件/lab1/Lab1_MUX_version/Lab1_MUX_version.srcs/sources_1/new/Lab1.v
+++ 实验文件/lab1/Lab1_MUX_version/Lab1_MUX_version.srcs/sources_1/new/Lab1.v
@@ -20,7 +20,49 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU(
+module ALU
+    #(parameter WIDTH = 32) 	// data width
+(
+    output reg [WIDTH-1:0] y,   // calculation result
+    output reg zf,              // zero sign
+    output reg cf,              // jinwei sign
+	output reg of,				// yichu
+	output reg sf,				// for signed cal
+	input [WIDTH-1:0] a,
+	input [WIDTH-1:0] b,
+	input [2:0] m				// type
+);
 
-    );
+	always @(*) begin
+		{zf,of,cf,sf} = 4'b0000;
+		case (m)
+			3'b000:  begin
+				{cf, y} = a + b;
+				of = (~a[WIDTH-1] & ~b[WIDTH-1] & y[WIDTH-1]) | (a[WIDTH-1] & b[WIDTH-1] & ~y[WIDTH-1]);
+				zf = ~|y;
+			end
+			3'b001: begin
+				{cf, y} = a - b;
+				of = (~a[WIDTH-1] & b[WIDTH-1] & y[WIDTH-1]) | (a[WIDTH-1] & ~b[WIDTH-1] & ~y[WIDTH-1]);
+				zf = ~|y;
+				sf = y[WIDTH-1];
+			end
+			3'b010: begin
+				y = a & b;
+				zf = ~|y;
+				sf = y[WIDTH-1];
+			end
+			3'b011: begin
+				y = a | b;
+				zf = ~|y;
+				sf = y[WIDTH-1];
+			end
+			3'b100: begin
+				y = a ^ b;
+				zf = ~|y;
+				sf = y[WIDTH-1];
+			end
+			default: y = a;
+		endcase
+	end
 endmodule

08539e234ed11cfc71d1029e37e643f3dd5a349d
Lapland_vlab <L.G.Stark@outlook.com>
2020-05-07 07:40:20 +0000

    upload tb_reg

---
+++ 实验文件/lab2/Lab_2/Lab_2.srcs/sim_1/new/tb_reg.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/05/06 15:04:40
+// Design Name: 
+// Module Name: tb_reg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_reg(
+
+    );
+endmodule

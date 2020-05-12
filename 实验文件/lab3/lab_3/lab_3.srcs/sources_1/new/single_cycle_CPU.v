a7ebfc0f50619a330ae63760f073984ab67495aa
Lapland_vlab <L.G.Stark@outlook.com>
2020-05-12 14:26:04 +0000

    update

---
+++ 实验文件/lab3/lab_3/lab_3.srcs/sources_1/new/single_cycle_CPU.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/05/12 12:05:36
+// Design Name: 
+// Module Name: sin_CPU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sin_CPU(
+
+    );
+endmodule

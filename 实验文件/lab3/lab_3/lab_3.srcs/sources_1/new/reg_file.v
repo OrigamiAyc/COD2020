e15d90d0b4c3e9b4d3bea01e6a60b2ab2fb4dbb1
Lapland_vlab <L.G.Stark@outlook.com>
2020-05-13 06:50:57 +0000

    update

--- 实验文件/lab3/lab_3/lab_3.srcs/sources_1/new/reg_file.v
+++ 实验文件/lab3/lab_3/lab_3.srcs/sources_1/new/reg_file.v
@@ -20,7 +20,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module reg_file(
+module reg_file
+	#(parameter WIDTH = 32)
+	(
+		input clk,
+		input [4:0] ra0,			// read port 0 addr
+		output [WIDTH-1:0] rd0,		// read port 0 data
+		input [4:0] ra1,			// read port 1 addr
+		output [WIDTH-1:0] rd1,		// read port 1 data
+		input [4:0] wa,				// write port addr
+		input we,					// write enable, valid at '1'
+		input [WIDTH-1:0] wd		// write port data
+	);
 
-    );
+	reg [WIDTH-1:0] reg_file [0:31];
+
+	assign rd0 = reg_file[ra0];
+	assign rd1 = reg_file[ra1];
+
+	always @(posedge clk) begin
+		if (we) begin
+			reg_file[wa] = wd;
+		end
+	end
 endmodule
--- 实验文件/lab3/lab_3/lab_3.srcs/sources_1/new/single_cycle_CPU.v
+++ 实验文件/lab3/lab_3/lab_3.srcs/sources_1/new/single_cycle_CPU.v
@@ -20,7 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+// suppose that there is a MAX_restrict of Instruction number: 256 (IP ROM volume)
 module sin_CPU(
+	input clk, rst
+	);
 
-    );
+	reg [7:0] PC; // program counter
+	reg [7:0] NPC; // new PC
+
+	wire [7:0] pc_addr;
+	wire [31:0] ins;
+
+	assign pc_addr = PC;
+
+	dist_inst_rom instruction(
+		.a(pc_addr),
+		.spo(ins)
+	);
+
+	always @(posedge clk) begin // pre-
+		NPC = PC + 1;
+	end
 endmodule
